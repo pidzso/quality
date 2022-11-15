37e30d3830ae338f78abc8dd3831aede84c64d65
AMD\PIDZSO <balazs.pejo@gmail.com>
2022-11-15 16:24:57 +0100

    Simulating two quality group

--- federated_main.py
+++ federated_main.py
@@ -32,12 +32,27 @@ if __name__ == '__main__':
     # load dataset and user groups
     train_dataset, test_dataset, user_groups = get_dataset(args)
 
-    # inject noise
+    # inject linear noise
     noise = torch.zeros([args.num_users])
     if args.noise_type == 'linear':
         # 1st participant: noise_size noise, last participant: 0 noise, in-between linearly decreasing noise level
         for n in range(args.num_users):
             noise[n] = args.noise_size - (n / (args.num_users - 1) * args.noise_size)
+        ran = torch.randint(10, [len(train_dataset.targets)])
+        for n in range(args.num_users):
+            ber = torch.bernoulli(torch.add(torch.zeros(len(train_dataset.targets)), noise[n]))
+            tmp = user_groups.copy()
+            tmp.pop(n, None)
+            for key in tmp.keys():
+                ber[list(tmp[key])] = 0
+            tmp = torch.as_tensor(train_dataset.targets)
+            train_dataset.targets = torch.where(ber > 0, ran, tmp)
+
+    # inject same noise
+    if args.noise_type.isnumeric():
+        # [1 - args.noise_type] participant: noise_size noise
+        for n in range(int(args.noise_type)):
+            noise[n] = args.noise_size
         ran = torch.randint(10, [len(train_dataset.targets)])
         for n in range(args.num_users):
             ber = torch.bernoulli(torch.add(torch.zeros(len(train_dataset.targets)), noise[n]))
@@ -127,9 +142,15 @@ if __name__ == '__main__':
             contributors[epoch, count] = idx
             count = count + 1
 
+        # save previous model
+        aggregate_old = global_model.state_dict()
+
         # update global weights
         if not (args.noise_type == 'freeride' and len(np.intersect1d(idxs_users, deviants)) == round(args.num_users * args.frac)):
-            if args.aggregate == 'avg' or args.aggregate == 'tm':
+            if args.aggregate == 'avg':
+                global_weights = average_weights(local_weights)
+                global_model.load_state_dict(global_weights)
+            elif args.aggregate == 'tm':
                 global_weights = median_weights(local_weights, args.robustness / args.num_users, device)
                 global_model.load_state_dict(global_weights)
             elif args.aggregate == 'KRUM':
@@ -140,6 +161,12 @@ if __name__ == '__main__':
                 # ToDo
                 global_weights = average_weights(local_weights)
                 global_model.load_state_dict(global_weights)
+
+        # save updated model
+        aggregate_new = global_model.state_dict()
+
+        # Shadow model training for single-round QI
+        # ToDo
 
         # measure the model test improvement epoch-wise
         test_acc, test_loss = test_inference(args, global_model, test_dataset)
